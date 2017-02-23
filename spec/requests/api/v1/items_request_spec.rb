6d3518175b4f6777acba5844bddb3c3b4baede96
Molly Brown <mollbrown@gmail.com>
2017-02-23 09:22:42 -0700

    add items api endpoint namespaced routes, items controller, and passing items index test

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "INDEX: gets all items" do
+    item_1 = Item.create!(name: "dingus", description: "some cheap crap")
+    item_2 = Item.create!(name: "pogo stick", description: "only the finest")
+
+    get "/api/v1/items"
+
+    items = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+
+    expect(items.count).to eq(2)
+  end
+
+  it "SHOW: gets one item by item id" do
+    item_1 = Item.create!(name: "dingus", description: "some cheap crap")
+    item_2 = Item.create!(name: "pogo stick", description: "only the finest")
+
+    get "/api/v1/items/#{item_1.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+
+    expect(item.count).to eq(1)
+    expect(item.id).to eq(item_1.id)
+    expect(item.name).to eq(item_1.name)
+    expect(item.description).to eq(item_1.description)
+
+
+  end
+
+
+end
