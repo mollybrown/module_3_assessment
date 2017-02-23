4e0120b3d5e8ab0c5063977c527d965f249257f8
Molly Brown <mollbrown@gmail.com>
2017-02-23 10:57:08 -0700

    add seach route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
+  get '/search', :action => 'search', :to => 'search'
+
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :destroy]
