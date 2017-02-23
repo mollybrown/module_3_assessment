0557e3ba56ec8f71dbfe7673fa5adc650622e59b
Molly Brown <mollbrown@gmail.com>
2017-02-23 10:10:30 -0700

    fix failing show action test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe "Items API" do
   it "INDEX: gets all items" do
-    item_1 = Item.create!(name: "dingus", description: "some cheap crap")
-    item_2 = Item.create!(name: "pogo stick", description: "only the finest")
+    item_1 = Item.create!(name: "dingus", description: "some cheap crap", image_url: "www.blank.com")
+    item_2 = Item.create!(name: "pogo stick", description: "only the finest", image_url: "www.blank.com")
 
     get "/api/v1/items"
 
@@ -14,8 +14,8 @@ describe "Items API" do
   end
 
   it "SHOW: gets one item by item id" do
-    item_1 = Item.create!(name: "dingus", description: "some cheap crap")
-    item_2 = Item.create!(name: "pogo stick", description: "only the finest")
+    item_1 = Item.create!(name: "dingus", description: "some cheap crap", image_url: "www.blank.com")
+    item_2 = Item.create!(name: "pogo stick", description: "only the finest", image_url: "www.blank.com")
 
     get "/api/v1/items/#{item_1.id}"
 
@@ -23,19 +23,18 @@ describe "Items API" do
 
     expect(response.status).to eq(200)
 
-    expect(item.count).to eq(1)
-    expect(item.id).to eq(item_1.id)
-    expect(item.name).to eq(item_1.name)
-    expect(item.description).to eq(item_1.description)
-    expect(item.image_url).to eq(nil)
+    expect(item["id"]).to eq(item_1.id)
+    expect(item["name"]).to eq(item_1.name)
+    expect(item["description"]).to eq(item_1.description)
+    expect(item["image_url"]).to eq(item_1.image_url)
 
     # expect(item.created_at).to eq(item_1.description)
     # expect(item.updated_at).to eq(nil)
   end
 
   it "DELETE: destroys an item's record" do
-    item_1 = Item.create!(name: "dingus", description: "some cheap crap")
-    item_2 = Item.create!(name: "pogo stick", description: "only the finest")
+    item_1 = Item.create!(name: "dingus", description: "some cheap crap", image_url: "www.blank.com")
+    item_2 = Item.create!(name: "pogo stick", description: "only the finest", image_url: "www.blank.com")
 
     delete "/api/v1/items/#{item_1.id}"
 
@@ -45,16 +44,16 @@ describe "Items API" do
   end
 
   it "CREATE: can create a new item" do
-    item_params = {name: "Yo-Yo", description: "can you walk the dog?"}
+    item_params = {name: "Yo-Yo", description: "can you walk the dog?", image_url: "www.blank.com"}
 
     post "/api/v1/items", params: {item: item_params}
     item = Item.last
 
     expect(response.status).to eq(201)
-    expect(item.id).to eq(item_params[:id])
-    expect(item.name).to eq(item_params[:name])
-    expect(item.description).to eq(item_params[:description])
-    expect(item.image_url).to eq(item_params[:image_url])
+    expect(item["id"]).to eq(item_params[:id])
+    expect(item["name"]).to eq(item_params[:name])
+    expect(item["description"]).to eq(item_params[:description])
+    expect(item["image_url"]).to eq(item_params[:image_url])
 
     # expect(item.created_at).to eq()
     # expect(item.updated_at).to eq()
